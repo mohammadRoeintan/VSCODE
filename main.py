9368ccaf81932cf62aa7e347a7ed993e1319cf8d
mohammadRoeintan <36801290+mohammadRoeintan@users.noreply.github.com>
2025-05-04 16:23:33 +0330

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from utils import build_graph, Data, split_validation
 from model import *
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--dataset', default='sample', help='dataset name: diginetica/yoochoose1_4/yoochoose1_64/sample')
+parser.add_argument('--dataset', default='yoochoose1_64', help='dataset name: diginetica/yoochoose1_4/yoochoose1_64/sample')
 parser.add_argument('--batchSize', type=int, default=100, help='input batch size')
 parser.add_argument('--hiddenSize', type=int, default=100, help='hidden state size')
 parser.add_argument('--epoch', type=int, default=10, help='the number of epochs to train for')  # کم شد برای سریع‌تر شدن
